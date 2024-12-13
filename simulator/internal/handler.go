7d7ed8f2671e0c2df033aa927a3f8b2c0cc5221d
Lucassamuel97 <lukassamuka88@gmail.com>
2024-12-13 10:37:26 -0300

    :sparkles: feat: Adicionar novos eventos de entrega e movimentação do motorista no simulador

--- simulator/internal/handler.go
+++ simulator/internal/handler.go
@@ -1,6 +1,10 @@
 package internal
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type RouteCreatedEvent struct {
 	EventName  string       `json:"event"`
@@ -24,6 +28,42 @@ type FreightCalculatedEvent struct {
 	Amount    float64 `json:"amount"`
 }
 
+func NewFreightCalculatedEvent(routeID string, amount float64) *FreightCalculatedEvent {
+	return &FreightCalculatedEvent{
+		EventName: "FreightCalculated",
+		RouteID:   routeID,
+		Amount:    amount,
+	}
+}
+
+type DeliveryStartedEvent struct {
+	EventName string `json:"event"`
+	RouteID   string `json:"route_id"`
+}
+
+func NewDeliveryStartedEvent(routeID string) *DeliveryStartedEvent {
+	return &DeliveryStartedEvent{
+		EventName: "DeliveryStarted",
+		RouteID:   routeID,
+	}
+}
+
+type DriverMovedEvent struct {
+	EventName string  `json:"event"`
+	RouteID   string  `json:"route_id"`
+	Lat       float64 `json:"lat"`
+	Lng       float64 `json:"lng"`
+}
+
+func NewDriverMovedEvent(routeID string, lat float64, lng float64) *DriverMovedEvent {
+	return &DriverMovedEvent{
+		EventName: "DriverMoved",
+		RouteID:   routeID,
+		Lat:       lat,
+		Lng:       lng,
+	}
+}
+
 func RouteCreatedHandler(event *RouteCreatedEvent, routeService *RouteService, mongoClient *mongo.Client) (*FreightCalculatedEvent, error) {
 	route := NewRoute(event.RouteID, event.Distance, event.Directions)
 	routeCreated, err := routeService.CreateRoute(route)
@@ -34,10 +74,19 @@ func RouteCreatedHandler(event *RouteCreatedEvent, routeService *RouteService, m
 	return freightCalculatedEvent, nil
 }
 
-func NewFreightCalculatedEvent(routeID string, amount float64) *FreightCalculatedEvent {
-	return &FreightCalculatedEvent{
-		EventName: "FreightCalculated",
-		RouteID:   routeID,
-		Amount:    amount,
+func DeliveryStartedHandler(event *DeliveryStartedEvent, routeService *RouteService, mongoClient *mongo.Client, ch chan *DriverMovedEvent) error {
+	route, err := routeService.GetRoute(event.RouteID)
+	if err != nil {
+		return err
 	}
+
+	driverMovedEvent := NewDriverMovedEvent(route.ID, 0, 0)
+	for _, direction := range route.Directions {
+		driverMovedEvent.RouteID = route.ID
+		driverMovedEvent.Lat = direction.Lat
+		driverMovedEvent.Lng = direction.Lng
+		ch <- driverMovedEvent
+		time.Sleep(1 * time.Second)
+	}
+	return nil
 }
