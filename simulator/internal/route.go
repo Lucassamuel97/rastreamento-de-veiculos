3e8e61f265b063991efacd951a32cc615ab7c734
Lucassamuel97 <lukassamuka88@gmail.com>
2024-12-13 10:10:25 -0300

    :sparkles: feat: Golang simulator - Implementar serviço de cálculo de frete e atualizar estrutura de rotas

--- simulator/internal/route.go
+++ simulator/internal/route.go
@@ -1,19 +1,72 @@
 package internal
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"fmt"
+	"math"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 type Directions struct {
-	Lat float64
-	Lng float64
+	Lat float64 `bson:"lat" json:"lat"`
+	Lng float64 `bson:"lng" json:"lng"`
 }
 
 type Route struct {
-	ID           string
-	Distance     int
-	Directions   []Directions
-	FreightPrice float64
+	ID           string       `bson:"_id" json:"id"`
+	Distance     int          `bson:"distance" json:"distance"`
+	Directions   []Directions `bson:"directions" json:"directions"`
+	FreightPrice float64      `bson:"freight_price" json:"freight_price"`
+}
+
+type FreightService struct{}
+
+func (fs *FreightService) Calculate(distance int) float64 {
+	return math.Floor((float64(distance)*0.15+0.3)*100) / 100
 }
 
 type RouteService struct {
-	mong *mongo.Client
+	mongo          *mongo.Client
+	FreightService *FreightService
+}
+
+func (rs *RouteService) CreateRoute(route Route) (Route, error) {
+
+	// Use FreightService to perform necessary calculations
+	freightCost := rs.FreightService.Calculate(route.Distance)
+	route.FreightPrice = freightCost
+	fmt.Printf("Calculated freight cost: %.2f\n", freightCost)
+
+	update := bson.M{
+		"$set": bson.M{
+			"distance":      route.Distance,
+			"directions":    route.Directions,
+			"freight_price": freightCost,
+		},
+	}
+
+	// Filter to find document by ID
+	filter := bson.M{"_id": route.ID}
+
+	// Upsert option to insert if not exists
+	opts := options.Update().SetUpsert(true)
+
+	// Perform the update or insert operation
+	_, err := rs.mongo.Database("routes").Collection("routes").UpdateOne(nil, filter, update, opts)
+
+	if err != nil {
+		return Route{}, err
+	}
+
+	return route, err
+}
+
+func (rs *RouteService) GetRoute(id string) (Route, error) {
+	var route Route
+	filter := bson.M{"_id": id}
+	err := rs.mongo.Database("routes").Collection("routes").FindOne(nil, filter).Decode(&route)
+	fmt.Printf("Found route: %+v\n", route)
+	return route, err
 }
